1e55b02de5756e0c3d0353546a94308610fd6837
Scott Charlton <charlton@usgs.gov>
2017-07-14 20:46:45 -0600

    added and updated case_initcc.cpp

--- unittests_cgnsfile/case_initcc.cpp
+++ unittests_cgnsfile/case_initcc.cpp
@@ -1,6 +1,10 @@
 #include "macros.h"
 
+#if defined(HAVE_QT)
 #include <QFile>
+#else
+#include "fs_copy.h"
+#endif
 
 #include <cgnslib.h>
 #include <iriclib.h>
@@ -12,7 +16,11 @@ extern "C" {
 void case_InitCC()
 {
 	remove("case_initcc.cgn");
+#if defined(HAVE_QT)
 	QFile::copy("case_nogrid.cgn", "case_initcc.cgn");
+#else
+	fs::copy("case_nogrid.cgn", "case_initcc.cgn");
+#endif
 
 	int fid;
 	int ier = cg_open("case_nogrid.cgn", CG_MODE_MODIFY, &fid);
