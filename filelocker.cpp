466619f40fb79e28a2c0f16356c97b184bf79215
Keisuke Inoue <keisuke.inoue@mizuho-ir.co.jp>
2017-09-05 20:21:36 +0900

    FileLocker implementation for windows borrowed from QLockFile.
    Plase refer to the following URL for original implementation.
    http://code.qt.io/cgit/qt/qtbase.git/tree/src/corelib/io/qlockfile_win.cpp?h=5.5
    QLockFilePrivate::tryLock_sys())

--- filelocker.cpp
+++ filelocker.cpp
@@ -9,6 +9,10 @@
 #define _stat stat
 #endif
 
+namespace {
+	static const int RETRY_INTERVAL_MS = 100;
+} // namespace
+
 FileLocker::Impl::Impl(const std::string& filename) :
 #ifdef _MSC_VER
 	m_handle {NULL},
@@ -51,16 +55,30 @@ bool FileLocker::lock()
 
 #ifdef _MSC_VER
 	// implementation for windows
-	impl->m_handle = ::CreateFileA(impl->m_filename.c_str(), GENERIC_WRITE, FILE_SHARE_WRITE, NULL, CREATE_ALWAYS, 0, NULL);
-	if (impl->m_handle == INVALID_HANDLE_VALUE) {
-		return false;
-	}
-	OVERLAPPED overlapped;
 
-	BOOL result = LockFileEx(impl->m_handle, LOCKFILE_EXCLUSIVE_LOCK, 0, 10, 0, &overlapped);
-	if (! result) {
-		return false;
+	while (1) {
+		std::string data = "lockfile";
+
+		SECURITY_ATTRIBUTES securityAtts = { sizeof(SECURITY_ATTRIBUTES), NULL, FALSE };
+		impl->m_handle = ::CreateFileA(impl->m_filename.c_str(), GENERIC_WRITE,
+																	 FILE_SHARE_READ, &securityAtts, CREATE_NEW, FILE_ATTRIBUTE_NORMAL, NULL);
+		if (impl->m_handle == INVALID_HANDLE_VALUE) {
+			goto RETRY;
+		}
+		DWORD bytesWritten = 0;
+		if (! WriteFile(impl->m_handle, data.c_str(), static_cast<DWORD> (data.size()), &bytesWritten, NULL) ||
+				! FlushFileBuffers(impl->m_handle))
+		{
+			goto RETRY;
+		}
+
+		// success
+		break;
+
+RETRY:
+		Sleep(RETRY_INTERVAL_MS);
 	}
+
 	impl->m_isLocked = true;
 	return true;
 
